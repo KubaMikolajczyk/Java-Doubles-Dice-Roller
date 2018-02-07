a2cf6ad94b57073216ac12b12e0892d89847b05a
KubaMikolajczyk <Qba600@gmail.com>
2018-02-08 00:39:00 +0100

    Implement basic menu loop in Application

--- DiceRoller/src/com/diceroller/Application.java
+++ DiceRoller/src/com/diceroller/Application.java
@@ -1,8 +1,38 @@
 package com.diceroller;
 
+import java.util.Scanner;
+
+/**
+ * Application class contain main loop of the program
+ */
+
 public class Application {
 
     public void startApp(){
-        
+        boolean is_running = true;
+        Scanner input = new Scanner(System.in);
+
+        while(is_running){
+            System.out.println("Please choose option from listed below: ");
+
+            int userMenuChoice = input.nextInt();
+            switch(userMenuChoice){
+                case 1:
+                    System.out.println("Roll dices");
+                    break;
+                case 2:
+                    System.out.println("Triples");
+                    break;
+                case 3:
+                    System.out.println("Exit");
+                    is_running = false;
+                    break;
+
+                    default:
+                        System.out.println("Please choose on of options listed above");
+                        break;
+
+            }
+            }
+        }
     }
-}
